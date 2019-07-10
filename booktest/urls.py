3146b5f7c8baffcf859c5262f2d5ccedfdccfe91
enzyme <zycctom@outlook.com>
2019-07-10 15:01:40 +0800

    数据库创建表

---
+++ booktest/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'enzyme'
+__date__ = '2019/7/10 1:57 PM'
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+    url(r'^/$')
+]
--- demo/settings.py
+++ demo/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'reqresp.apps.ReqrespConfig',
+    'booktest.apps.BooktestConfig',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +78,12 @@ WSGI_APPLICATION = 'demo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'NAME': 'django_demo',
     }
 }
 
