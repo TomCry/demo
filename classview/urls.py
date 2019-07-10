27af7fcaf1fde75e9e43fdfaeba8ed6ec3cf5f16
enzyme <zycctom@outlook.com>
2019-07-10 10:05:03 +0800

    类视图装饰器第二种方法

--- classview/urls.py
+++ classview/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^demoview/$', views.my_decorator(views.DemoView.as_view())),
+    # url(r'^demoview/$', views.my_decorator(views.DemoView.as_view())),
+    url(r'^demoview/$', views.DemoView.as_view()),
 ]
--- classview/views.py
+++ classview/views.py
@@ -1,24 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import View
+from django.utils.decorators import method_decorator
 
 
 # Create your views here.
 
-class DemoView(View):
-
-    def get(self, request):
-        return HttpResponse('get page')
-
-    def post(self, request):
-        return HttpResponse(' post page')
-
 
 def my_decorator(view_func):
-
     def wrapper(request, *args, **kwargs):
         print("装饰器被调用")
         print(request.path)
         return view_func(request, *args, **kwargs)
 
     return wrapper
+
+
+class DemoView(View):
+
+    @method_decorator(my_decorator)
+    def get(self, request):
+        return HttpResponse('get page')
+
+    def post(self, request):
+        return HttpResponse(' post page')
